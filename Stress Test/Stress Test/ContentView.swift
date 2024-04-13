71c9e3e3aa69c0718699671c3fa34ffa8737f6b4
NyasaLuharuka <73618884+NyasaLuharuka@users.noreply.github.com>
2024-04-12 21:45:19 -0400

    Add files via upload

--- Stress Test/Stress Test/ContentView.swift
+++ Stress Test/Stress Test/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     @State var imageName = "cookies"
+    @State var x = 0.0;
+    @State var y = 0.0;
     var body: some View {
         ZStack {
             Color(#colorLiteral(red: 0.930326283, green: 0.9053675532, blue: 0.8665257096, alpha: 1))
@@ -27,17 +29,24 @@ struct ContentView: View {
                 .padding(/*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/)
                 .padding(/*@START_MENU_TOKEN@*/.bottom/*@END_MENU_TOKEN@*/, 500)
                 .foregroundColor(Color(#colorLiteral(red: 0.7172502875, green: 0.2730030119, blue: 0.3158176243, alpha: 1)))
-            
-            Button("Start") {
-                check()
+            VStack {
+                Button("Start") {
+                    check()
+                }
+                .buttonStyle(.bordered)
+                .tint(Color(#colorLiteral(red: 0.63688308, green: 0.5869231224, blue: 0.5352438688, alpha: 1)))
+                .font(.title)
+                .hoverEffect(/*@START_MENU_TOKEN@*/.lift/*@END_MENU_TOKEN@*/)
+                .fontWeight(.heavy)
+                .padding(.top, 450)
+                .shadow(radius: 10)
+                
+                Text("Score")
+                    .font(.title)
+                    .fontWeight(.medium)
+                    .padding(.top)
             }
-            .buttonStyle(.bordered)
-            .tint(Color(#colorLiteral(red: 0.63688308, green: 0.5869231224, blue: 0.5352438688, alpha: 1)))
-            .font(.title)
-            .hoverEffect(/*@START_MENU_TOKEN@*/.lift/*@END_MENU_TOKEN@*/)
-            .fontWeight(.heavy)
-            .padding(.top, 450)
-            .shadow(radius: 10)
+            
             
             
             Image("cookieButton")
@@ -45,7 +54,8 @@ struct ContentView: View {
                 .aspectRatio(contentMode: .fit)
                 .padding(.all)
                 .frame(width: 100, height: 100)
-                .position(x:0, y:750)
+                .position(x:x, y:y)
+                .shadow(radius: 10)
 
 
         }
@@ -53,13 +63,17 @@ struct ContentView: View {
     }
     
     func check() {
-        print("Cookie")
+        /*
         if (imageName == "cookies") {
             imageName = "cookieButton"
         }
         else {
             imageName = "cookies"
         }
+         */
+        
+        x = Double.random(in: 10...350)
+        y = Double.random(in: 10...750)
     }
 }
 
